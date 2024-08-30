1837f329fe4e9f8c28e2f44d0f1c60b11b91139e
I-Atlas <54947380+I-Atlas@users.noreply.github.com>
2024-08-30 14:40:30 +0300

    fix error

--- src/Root.tsx
+++ src/Root.tsx
@@ -6,7 +6,6 @@ import { ErrorBoundary } from './ErrorBoundary';
 
 const ErrorBoundaryError: FC<{ error: unknown }> = ({ error }) => (
   <div>
-    <p>An unhandled error occurred:</p>
     <blockquote>
       <code>{error instanceof Error ? error.message : typeof error === 'string' ? error : JSON.stringify(error)}</code>
     </blockquote>
